b340102187cc7f58cd7bd78e2a0f32244663ac84
Maria Trishch <magrateya@meta.ua>
2020-11-08 21:06:42 +0200

    fix fetch

--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,37 +1,36 @@
-const BASE_URL = 'https://newsapi.org/v2/';
-const options = {
-        headers: {
-            Authorization: '2f479ca51904464eaecc59acc7478e76',
-        },
-    };
-
 export default class NewsApiService {
-    constructor() {
-        this.searchInput = '';
-        this.page = 1;
-    }
+  constructor() {
+    this.searchInput = '';
+    this.page = 1;
+  }
 
-    fetchArticles() {
-        console.log(this);
-        
+  fetchArticles() {
+    const BASE_URL = 'https://newsapi.org/v2/';
+    const options = {
+      headers: {
+        Authorization: '2f479ca51904464eaecc59acc7478e76',
+      },
+    };
 
     const url = `${BASE_URL}everything?qInTitle=${this.searchInput}&language=en&pageSize=5&page=${this.page}&apiKey=${options.headers.Authorization}`;
 
-        return fetch(url, options).then(responce => responce.json()).then(({articles}) => {
-            this.page += 1;
-            return articles;
-    });
-    }
+    return fetch(url, options)
+      .then(responce => responce.json())
+      .then(({ articles }) => {
+        this.page += 1;
+        return articles;
+      });
+  }
 
-    restPage() {
-        this.page = 1;
-    }
+  restPage() {
+    this.page = 1;
+  }
 
-    get query() {
-       return this.searchInput;
-    }
+  get query() {
+    return this.searchInput;
+  }
 
-    set query(newQuery) {
-        this.searchInput = newQuery;
-    }
+  set query(newQuery) {
+    this.searchInput = newQuery;
+  }
 }
